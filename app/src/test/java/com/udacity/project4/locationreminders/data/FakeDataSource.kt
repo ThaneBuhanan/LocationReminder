a2ecda7e847ac23b819be269498d95fb748c57ae
ThaneBuhanan <thanebuhanan@gmail.com>
2023-03-29 21:01:45 -0700

    SaveReminderViewModelTest.kt created and passes.

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -1,28 +1,34 @@
 package com.udacity.project4.locationreminders.data
 
+import androidx.annotation.VisibleForTesting
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
-//Use FakeDataSource that acts as a test double to the LocalDataSource
 class FakeDataSource : ReminderDataSource {
+    private val list = mutableListOf<ReminderDTO>()
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    @VisibleForTesting
+    var shouldSucceed: Boolean = true
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        return when (shouldSucceed) {
+            true -> Result.Success(list)
+            else -> Result.Error("It failed!", 500)
+        }
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        list.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        return when (shouldSucceed) {
+            true -> Result.Success(list.first { it.id == id })
+            else -> Result.Error("It failed!", 500)
+        }
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        list.clear()
     }
-
-
 }
